495617e41be85e2b95dbd7b4587e192ac78a1d3c
Shanchikkaj <shanchikkajeganathan@gmail.com>
2025-05-31 16:21:02 +0530

    add update function

--- Side-scoller/script.js
+++ Side-scoller/script.js
@@ -10,17 +10,62 @@ window.addEventListener('load', function(){
       constructor(){
         this.keys = [];
         window.addEventListener('keydown', e =>{
-            console.log(e.key);
-            if(e.key === "ArrowDown"){
+            
+            if( (e.key === 'ArrowDown'|| 
+              e.key === 'ArrowUp' || 
+              e.key === 'ArrowLeft' ||
+              e.key === 'ArrowRight')
+             
+              && this.keys.indexOf(e.key) === -1){
               this.keys.push(e.key);
             }
-            console.log(e.key, this.keys);
+           
         });
+        window.addEventListener('keyup', e =>{
+          if(e.key === 'ArrowDown' ||
+            e.key === 'ArrowUp' || 
+              e.key === 'ArrowLeft' ||
+              e.key === 'ArrowRight'
+          ){
+            this.keys.splice(this.keys.indexOf(e.key),1);
+          }
+          
+        });
+      
       }
    }
 
    class Player{
+       constructor(gameWidth, gameHeight){
+        this.gameWidth = gameWidth;
+        this.gameHeight = gameHeight;
+        this.width = 200;
+        this.height = 200;
+        this.x = 10;
+        this.y = this.gameHeight -this.height;
+        this.image = document.getElementById('playerImage');
+        this.frameX =0;
+        this.framey = 0;
+        this.speed = 0;
+       }
+       draw(context){
+        context.fillStyle = 'white';
+        context.fillRect(this.x,this.y,this.width, this.height);
+        context.drawImage(this.image, 5*this.width ,1*this.height, this.width, this.height, this.x, this.y, this.width, this.height);
+       }
+       update(input){
+        //horizontal movement
+        this.x += this.speed;
+        if(input.keys.indexOf('ArrowRight') > -1) {
+          this.speed = 5;
+        } else if (input.keys.indexOf('ArrowLeft') > -1){
+          this.speed = -5;
 
+        }
+        else{
+          this.speed =0;
+        }
+       }
    }
 
    class Background{
@@ -41,9 +86,16 @@ window.addEventListener('load', function(){
    }
 
    const input = new InputHandler();
+   const player = new Player(canvas.width, canvas.height);
+   player.draw(ctx);
+   player.update(input);
 
    function animate(){
       //update and drawing game
-
+      ctx.clearRect(0,0,canvas.width, canvas.height);
+      player.draw(ctx);
+      player.update();
+      requestAnimationFrame(animate);
    }
+   animate();
 });
