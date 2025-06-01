c6e889301e56fbe2cacc543d408c9a7a72fb7617
Shanchikkaj <shanchikkajeganathan@gmail.com>
2025-06-01 14:16:10 +0530

    Enemy Added

--- Side-scoller/script.js
+++ Side-scoller/script.js
@@ -3,6 +3,7 @@ window.addEventListener('load', function(){
     const ctx = canvas.getContext('2d');
     canvas.width = 800;
     canvas.height = 720;
+    let enemies = [];
 
 
    class InputHandler{
@@ -47,6 +48,8 @@ window.addEventListener('load', function(){
         this.frameX =0;
         this.framey = 0;
         this.speed = 0;
+        this.vy =0;
+        this.weight =1;
        }
        draw(context){
         context.fillStyle = 'white';
@@ -54,32 +57,94 @@ window.addEventListener('load', function(){
         context.drawImage(this.image, 5*this.width ,1*this.height, this.width, this.height, this.x, this.y, this.width, this.height);
        }
        update(input){
-        //horizontal movement
-        this.x += this.speed;
+      
         if(input.keys.indexOf('ArrowRight') > -1) {
           this.speed = 5;
         } else if (input.keys.indexOf('ArrowLeft') > -1){
           this.speed = -5;
-
+        } else if (input.keys.indexOf('ArrowUp') > -1 && this.onGround()){
+          this.vy -= 32;
         }
         else{
           this.speed =0;
         }
+          //horizontal movement
+        this.x += this.speed;
         if (this.x < 0) this.x = 0;
         else if(this.x > this.gameWidth - this.width) this.x = this.gameWidth - this.width 
+        // vertical movement
+        this.y += this.vy;
+        if(!this.onGround()){
+          this.vy += this.weight;
+          this.framey = 1;
+        }else{
+          this.vy =0;
+          this.framey = 0;
+        }
+        if(this.y > this.gameHeight -this.height) this.y =this.gameHeight -this.height
+       }
+       onGround(){
+        return this.y >= this.gameHeight - this.height;
        }
    }
 
    class Background{
+    constructor(gameWidth,gameHeight){
+      this.gameWidth = gameWidth;
+      this.gameHeight = gameHeight;
+      this.image = document.getElementById('backgroundImage');
+      this.x =0;
+      this.y = 0;
+      this.width = 2400;
+      this.height = 720;
+      this.speed =7;
+    }
+    draw(context){
+      context.drawImage(this.image,this.x,this.y, this.width,this.height);
+      context.drawImage(this.image,this.x + this.width -this.speed,this.y, this.width,this.height);
+    }
+    update(){
+       this.x -= this.speed;
+       if(this.x < 0 -this.width) this.x =0;
+    }
 
    }
 
    class enemy{
+    constructor(gameWidth, gameHeight){
+      this.gameWidth = gameWidth;
+      this.gameHeight = gameHeight;
+      this.width = 160;
+      this.height = 119;
+      this.image = document.getElementById('enemyImage');
+      this.x =this.gameWidth;
+      this.y =this.gameHeight - this.height;
+      this.frameX = 0;
+      this.speed = 8;
+
+    }
+    draw(context){
+      context.drawImage(this.image,  this.frameX*this.width,0,this.width,this.height,this.x,this.y, this.width, this.height);
+    }
+    update(){
+      this.x-= this.speed;
+    }
 
    }
     
-   function handleEnemies(){
+   function handleEnemies(deltaTime){
      // add and remove enemies
+      if(enemyTimer > enemyInterval + randomEnemyInterval){
+         enemies.push(new enemy(canvas.width, canvas.height));
+         randomEnemyInterval = Math.random()*1000 + 500;
+         enemyTimer = 0;
+      } else{
+        enemyTimer += deltaTime;
+      }
+     enemies.forEach(enemy =>{
+      enemy.draw(ctx);
+      enemy.update();
+     })
    }
 
    function displayStatusText(){
@@ -89,15 +154,23 @@ window.addEventListener('load', function(){
 
    const input = new InputHandler();
    const player = new Player(canvas.width, canvas.height);
-   player.draw(ctx);
-   player.update(input);
+   const background = new Background(canvas.width, canvas.height);
+   let lastTime =0;
+   let enemyTimer = 0;
+   let enemyInterval = 1000;
+   let randomEnemyInterval = Math.random()*1000 + 500;
 
-   function animate(){
+   function animate(timestamp){
       //update and drawing game
+      const deltaTime = timestamp -lastTime;
+      lastTime = timestamp;
       ctx.clearRect(0,0,canvas.width, canvas.height);
+       background.draw(ctx);
+       //background.update();
       player.draw(ctx);
       player.update(input);
+      handleEnemies(deltaTime)
       requestAnimationFrame(animate);
    }
-   animate();
+   animate(0);
 });
